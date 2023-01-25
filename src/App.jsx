db1f34e6a0565a0b21559f337d7b0250445f2625
Psicomago <carlosaravena.coach@gmail.com>
2023-01-25 03:25:51 -0300

    button component creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button } from './components/Button'
+import { TiArrowBackOutline } from "react-icons/ti";
+import { TiArrowForwardOutline } from "react-icons/ti";
+import './sass/App.scss';
+// import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonId, setPokemonId] = useState(1)
+
+  function prevClick(){
+    (pokemonId === 1)?
+    setPokemonId(1):
+    setPokemonId(pokemonId -1)
+  }
+
+  function nextClick(){
+    setPokemonId(pokemonId +1)
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hola mundos</h1>
+      <>
+        {/* tarjetas */}
+        <div className="buttons-container">
+          <Button 
+            icon={<TiArrowBackOutline />} 
+            handleClick={prevClick}
+          />
+          {pokemonId}
+          <Button 
+            icon={<TiArrowForwardOutline />} 
+            handleClick={nextClick}
+          />
+        </div>
+      </>
+      
     </div>
   )
 }
